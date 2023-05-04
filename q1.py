412419b6dece21fb012fd751c0c0843533c52bb8
Marie Shotts <mshotts2026@kehillahstudent.org>
2023-05-04 09:50:25 -0700

    change to make a plot of census & population

--- q1.py
+++ q1.py
@@ -1,1 +1,14 @@
-print("hello world")
+import bs4
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+
+tables = pd.read_html(
+    "https://en.wikipedia.org/wiki/Minnesota",
+    match = "Historical population"
+)
+table = tables[0]
+df = table[["Census", "Pop."]]
+df = df.iloc[0:-2].astype("int")
+df.plot(x = "Census", y = "Pop.")
+plt.show()
